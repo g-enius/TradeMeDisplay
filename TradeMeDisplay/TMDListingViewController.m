07ba91b6439bc621e2a68f459f799d0b580ca842
wangchao <wang_chao@corp.netease.com>
2017-03-06 16:49:47 +1300

    added gif

--- TradeMeDisplay/TMDListingViewController.m
+++ TradeMeDisplay/TMDListingViewController.m
@@ -71,4 +71,8 @@
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 @end
